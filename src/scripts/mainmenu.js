f3ce0226c7ec45999f27de5fcd60a47e0e21bc4f
BigBen83 <benjamin.toubhans@orange.fr>
2023-07-28 21:13:04 +0200

    230728 navigation

--- src/scripts/mainmenu.js
+++ src/scripts/mainmenu.js
@@ -1,14 +1,1 @@
-const navigation = document.querySelector(".navigation");
-let lastScrollValue = 0;
 
-document.addEventListener('scroll', () => {
-    let top = document.documentElement.scrollTop;
-    if(lastScrollValue < top){
-        navigation.classList.add("hidden");
-    }
-    else
-    {
-        navigation.classList.remove("hidden");
-    }
-    lastScrollValue = top;
-})
