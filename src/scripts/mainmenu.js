7627376c562e9e2142a3c1f19b8f9c3a97a711ab
BigBen83 <benjamin.toubhans@orange.fr>
2023-08-01 21:31:23 +0200

    230801 Menu mobile

--- src/scripts/mainmenu.js
+++ src/scripts/mainmenu.js
@@ -1,1 +1,10 @@
+const navLinks = Array.from(document.querySelectorAll('.nav-link'));
+const checkbox = document.querySelector('.checkbox-toggler');
 
+console.log(navLinks);
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        checkbox.checked = false;
+    })
+})
